dd3c978aecbffc9d950b8bdc54d2a1c0725029b4
unknown <you@example.com>
2026-02-05 11:44:49 +0300

    assignment

---
+++ assignment.py
@@ -0,0 +1,11 @@
+# Write a python program that is able to calculate the BMI of a person weight 78kg height 1.75m BMI= (weight) / (height squared)
+w = 78
+h = 1.75 ** 2
+BMI = (w / h)
+print("The BMI is: ", BMI)
+
+# area is lenght * width
+length = 48
+width = 25
+area = (length * width)
+print("the area is: ", area)
---
+++ lesson1d.py
@@ -0,0 +1,8 @@
+# Create a python program that is able to calculate the simple interst given the principle as 50000, rate as 5% and time as 8 years. SI = (p * r * t )/100
+p = 50000
+r = 5
+t = 8
+SI = (p * r * t)/100
+print("The simple interest is: ", SI)
+
+# Write a python programme that is able to calculate the BMI 
